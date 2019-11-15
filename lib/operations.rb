912bf8d8df62de5716cfc46bdfb3b74b9d6be2a0
dagonyclark <sean@dagony-clark.com>
2019-11-15 15:00:16 -0500

    Revert "Done."
    This reverts commit befebfd12f746f2699315dc8859c04c1d518f8be.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,12 @@
-require 'pry'
-
 def unsafe?(speed)
-  if speed > 60
-		return true
-	elsif speed < 40
-		return true
-	else
-		return false
-	end
-	#return speed < 40
+
 end
 
 
 
 def not_safe?(speed)
-  speed > 60 ? true : speed < 40 ? true : false
+	
 end
+	
+
+
